8b45482d432bde138c1b5d8e8ce53d4b98e12918
Nikolay Chunosov <N.Chunosov@yandex.ru>
2019-04-18 16:16:20 +0700

    adjust help build script to MacOS

--- help/make.sh
+++ help/make.sh
@@ -4,7 +4,8 @@
 #
 
 # Check if Qt is in PATH
-HELP_TOOL_VER="$(qcollectiongenerator -v)"
+HELP_TOOL=qcollectiongenerator
+HELP_TOOL_VER="$(${HELP_TOOL} -v)"
 if [[ -z "${HELP_TOOL_VER}" ]]; then
   echo
   echo "ERROR: qmake is not found in PATH."
@@ -23,8 +24,17 @@ cd ${SCRIPT_DIR}/..
 SOURCE_DIR=./help
 TARGET_DIR=./out/help
 
+if [ "$(uname)" == "Darwin" ]; then
+  HELP_TOOL_PATH="$(which ${HELP_TOOL})"
+  HELP_TOOL_DIR="$(dirname ${HELP_TOOL_PATH})"
+  ASSISTANT=${HELP_TOOL_DIR}/Assistant.app/Contents/MacOS/Assistant
+else
+  ASSISTANT=assistant
+fi
+
 python3 -m sphinx -b qthelp ${SOURCE_DIR} ${TARGET_DIR}
 
-qcollectiongenerator ${TARGET_DIR}/rezonator2.qhcp
+${HELP_TOOL} ${TARGET_DIR}/rezonator2.qhcp
 
-assistant -collectionFile ${TARGET_DIR}/rezonator2.qhc
+${ASSISTANT} -collectionFile ${TARGET_DIR}/rezonator2.qhc
+
